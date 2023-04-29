c39cc85f8d2cfc12aba3cd7298c65845381c468c
00shivani <shivanisingh13@icloud.com>
2023-04-29 16:38:43 -0400

    final changes

--- Assets/script.js
+++ Assets/script.js
@@ -1,38 +1,85 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
- var lengthOfPassword = parseInt(prompt("what is the length of your password?"))
- console.log(lengthOfPassword)
- //isNaN()
- if(isNaN(lengthOfPassword) ){
-  alert("Please enter numerical value")
-  return "please try again"
- }
- if(lengthOfPassword < 8){
-  alert("Invalid, must be greater then 8")
-  return "please try again"
- }
- if(lengthOfPassword > 128){
-  alert("Invalid, must be less than 128")
-  return "please try again"
- }
- var specialCharacters = confirm("do you want special characters in your password?")
- console.log(specialCharacters)
- var numbers = confirm("do you want numbers in your password?")
- console.log(numbers)
- var uppercase = confirm("do you want uppercase letters in your password")
- console.log(uppercase)
- var lowercase = confirm("do you want lowercase letters in your password")
-console.log(lowercase)
-}
-// Write password to the #password input
-function writePassword(){
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var output = document.querySelector('#password');
 
-  passwordText.value = password;
+var specialCharacters = ['!', '@', '#', '$', '%', '&', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '^', '_', '`', '{', '|', '}', '~'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+function generatePassword(optionsObj) {
+  var passwordchoices = [];
+  var password = '';
+
+  if (optionsObj.specialCharacters) {
+    passwordchoices = passwordchoices.concat(specialCharacters);
+  }
+
+  if (optionsObj.numbers) {
+    passwordchoices = passwordchoices.concat(numbers);
+  }
+
+  if (optionsObj.uppercase) {
+    passwordchoices = passwordchoices.concat(uppercase);
+  }
+
+  if (optionsObj.lowercase) {
+    passwordchoices = passwordchoices.concat(lowercase);
+  }
+
+  for (var i = 0; i < optionsObj.length; i++) {
+    var randomSelection = passwordchoices[ Math.floor(Math.random () * passwordchoices.length) ];
+
+    password += randomSelection;
+  }
+
+  console.log(password);
+  output.innerText = password;
+}
+
+
+function getOptions() {
+  var lengthOfPassword = parseInt(prompt("what is the length of your password? Must be a numerical value of 8 characters or more."))
+    console.log(lengthOfPassword)
+ //isNaN()
+  if(isNaN(lengthOfPassword) ){
+      alert("Please enter numerical value")
+      return "please try again"
+  }
+  if(lengthOfPassword < 8){
+    alert("Invalid, must be greater then 8")
+    return "please try again"
+  }
+  if(lengthOfPassword > 128){
+    alert("Invalid, must be less than 128")
+    return "please try again"
+  }
+
+ var optionspecialCharacters = confirm("do you want special characters in your password?");
+  console.log(optionspecialCharacters);
+
+ var optionnumbers = confirm("do you want numbers in your password?");
+  console.log(optionnumbers);
+
+ var optionuppercase = confirm("do you want uppercase letters in your password");
+  console.log(optionuppercase);
+
+ var optionlowercase = confirm("do you want lowercase letters in your password");
+  console.log(optionlowercase);
+
+
+  var passwordOptions = {
+    specialCharacters: optionspecialCharacters,
+    numbers: optionnumbers,
+    uppercase: optionuppercase,
+    lowercase: optionlowercase,
+    length: lengthOfPassword
+  };
+
+  generatePassword(passwordOptions);
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", getOptions);
